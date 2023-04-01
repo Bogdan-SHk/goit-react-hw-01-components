7670c3e94bd69fc578a9536f5a61908d5df0538f
Bogdan-SHk <bogdan.shkolnik@gmail.com>
2023-04-01 14:14:08 +0300

    Виконав завдання 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from "components/Profile/Profile";
+import { Statistics } from 'components/Statistics/Statistics';
 import user from '../user.json';
+import data from '../data.json';
 
 export const App = () => {
   const { username, tag, location, avatar, stats, } = user;
@@ -14,6 +16,7 @@ export const App = () => {
         views={stats.views}
         likes={stats.likes}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+
+      <ul className={css.stat_list}>
+        {stats.map(stat => (
+          <li
+            key={stat.id}
+            className={css.item}
+            style={{
+              backgroundColor: getRandomHexColor(),
+            }}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
